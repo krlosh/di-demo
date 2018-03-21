16757d1613ae2834a3688f51f44d539a96ac53bc
krlosh <krlosh@gmail.com>
2018-03-21 18:50:09 +0100

    Examples using qualifiers

--- src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/didemo/controllers/ConstructorInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.didemo.controllers;
 
 import guru.springframework.didemo.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -9,7 +10,7 @@ public class ConstructorInjectedController {
 
     private GreetingService greetingService;
     @Autowired //Este es opcional pero ayuda
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGreetingServiceImpl") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
---
+++ src/main/java/guru/springframework/didemo/services/ConstructorGreetingServiceImpl.java
@@ -0,0 +1,13 @@
+package guru.springframework.didemo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorGreetingServiceImpl implements GreetingService {
+
+
+    @Override
+    public String sayGreeting() {
+        return "Hello - I was injected by the constructor";
+    }
+}
