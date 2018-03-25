a97b3d043f490bcf6d32833138eb10f80e49c650
krlosh <krlosh@gmail.com>
2018-03-25 12:28:00 +0200

    Using Primarybean

--- src/main/java/guru/springframework/didemo/DiDemoApplication.java
+++ src/main/java/guru/springframework/didemo/DiDemoApplication.java
@@ -15,7 +15,7 @@ public class DiDemoApplication {
 
         ApplicationContext context = SpringApplication.run(DiDemoApplication.class, args);
         MyController myController = context.getBean("myController", MyController.class);
-        myController.hello();
+        System.out.println(myController.hello());
         System.out.println(context.getBean(PropertyInjectedController.class).sayHello());
         System.out.println(context.getBean(SetterInjectedController.class).sayHello());
         System.out.println(context.getBean(ConstructorInjectedController.class).sayHello());
