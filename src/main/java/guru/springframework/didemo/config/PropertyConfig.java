83bb333863dec12b5d657c0a5abd68938e72a968
krlosh <krlosh@gmail.com>
2018-04-15 11:37:43 +0200

    Environment variables injection

--- src/main/java/guru/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/guru/springframework/didemo/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package guru.springframework.didemo.config;
 
 import guru.springframework.didemo.examplebeans.FakeDatasource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -23,7 +28,10 @@ public class PropertyConfig {
     @Bean
     public FakeDatasource fakeDatasource(){
         FakeDatasource fakeDatasource = new FakeDatasource();
-        fakeDatasource.setUsername(user);
+        //si la environment property fuera GURU_USERNAME, no sería necesario inyectar Environment por que
+        //la variable de entorno GURU_USERNAME sobre-escribiría la definida en datasource.properties.
+        String envUser = env.getProperty("USERNAME");
+        fakeDatasource.setUsername(envUser);
         fakeDatasource.setPassword(password);
         fakeDatasource.setDburl(url);
         return fakeDatasource;
